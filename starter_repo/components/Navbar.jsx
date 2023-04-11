cb47bf5776442a15bbc86357308dae4e80963290
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-11 20:38:20 +0530

    .

--- starter_repo/components/Navbar.jsx
+++ starter_repo/components/Navbar.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import styles from "../styles"
+
+import { navVariants } from "../utils/motion"
+
+import {motion} from "framer-motion"
+
 const Navbar = () => (
   <nav>
     navbar
