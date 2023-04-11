1bcfb4b6a7e3d7dd0afadcf31820413dbb04ba55
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-11 21:25:19 +0530

    hero.jsx

--- starter_repo/sections/Hero.jsx
+++ starter_repo/sections/Hero.jsx
@@ -1,8 +1,19 @@
-'use client';
+"use client";
+
+import styles from "../styles";
+
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
+
+import { motion } from "framer-motion";
 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+    />
   </section>
 );
 
