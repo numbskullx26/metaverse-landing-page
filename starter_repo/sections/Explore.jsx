e9b7f6354e01f8a49d33e10a247987a8c3dad347
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-12 21:14:40 +0530

    explore.jsx 03

--- starter_repo/sections/Explore.jsx
+++ starter_repo/sections/Explore.jsx
@@ -1,41 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { use, useState } from "react";
 import { TypingText, ExploreCard, TitleText } from "../components";
-import { exploreworlds } from "../constants";
+import { exploreWorlds } from "../constants";
 import styles from "../styles";
 
 import { staggerContainer } from "../utils/motion";
 
 import { motion } from "framer-motion";
 
-const Explore = () => (
-  <section className={`${styles.paddings}`} id="explore">
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-      <TypingText title="| The World" textStyles="text-center" />
-      <TitleText title={<>Choose the World you want <br className="md:block hidden" /> to explore</>}
-      textStyles= "text-center" />
-      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
-        {exploreworlds.map(world,index) =>(
-          <ExploreCard 
-            key={world.id}
-            {...world}
-            index={index}
-            active={active}
-            handleClick={setActive}
-          />
-        )}
+const Explore = () => {
+  const [active, setActive] = useState("world-2");
 
-
-      </div>
-    </motion.div>
-  </section>
-);
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the World you want <br className="md:block hidden" /> to
+              explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
+};
 
 export default Explore;
