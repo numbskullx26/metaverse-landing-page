481e9f5ea9f7624c7b3f1978d333d57a4904acbd
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-13 11:41:22 +0530

    Update ExploreCard.jsx

--- starter_repo/components/ExploreCard.jsx
+++ starter_repo/components/ExploreCard.jsx
@@ -1,9 +1,26 @@
-'use client';
+"use client";
 
-const ExploreCard = () => (
-  <div>
-    Explore Card
-  </div>
+import styles from "../styles";
+
+import { fadeIn } from "../utils/motion";
+
+import { motion } from "framer-motion";
+
+const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
+  <motion.div
+    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+    className={`relative ${
+      active === id ? "lg:flex-[3.5] f;ex-[10]" : "lg:flex-[0.5] flex-[2]"
+    }
+      flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer'}
+      `}
+  >
+    <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover rounded-[24px]" />
+      
+  </motion.div>
 );
 
 export default ExploreCard;
