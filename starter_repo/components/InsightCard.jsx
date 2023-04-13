dc1bdd8b68fcd1f329750aa7d0690dc025dbe8d0
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-13 12:50:36 +0530

    Update InsightCard.jsx

--- starter_repo/components/InsightCard.jsx
+++ starter_repo/components/InsightCard.jsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { fadeIn } from '../utils/motion';
+import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({ imgUrl, title, subtitle, index }) => (
   <motion.div
-    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
     <img
@@ -24,12 +24,15 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
         </p>
       </div>
 
-      <div
-        className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white"
-      >
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
         <img
           src="/arrow.svg"
           alt="arrow"
           className="w-[40%] h-[40%] object-contain"
         />
       </div>
+    </div>
+  </motion.div>
+);
+
+export default InsightCard;
