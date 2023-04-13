2afe2633c03c5d59657b5d6f2c757c48fec6b866
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-13 11:55:40 +0530

    Update ExploreCard.jsx

--- starter_repo/components/ExploreCard.jsx
+++ starter_repo/components/ExploreCard.jsx
@@ -28,12 +28,15 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       <div className = "absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
 
         <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
-          
+
           <img 
             src="/headset.svg"
             alt="headset"
             className="w-1/2 h-1/2 obejct-contain"/>
         </div>
+        <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
+          Enter the Metaverse
+        </p>
       </div>
     )}
   </motion.div>
