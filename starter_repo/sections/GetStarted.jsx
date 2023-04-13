ea9c18ea6f8c33eaf6a7d634665d42477a93e0f6
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-13 12:22:05 +0530

    Update GetStarted.jsx

--- starter_repo/sections/GetStarted.jsx
+++ starter_repo/sections/GetStarted.jsx
@@ -1,9 +1,22 @@
 'use client';
 
-const GetStarted = () => (
-  <section>
-    Get Started Section
-  </section>
-);
+import { TypingText, StartSteps, TitleText } from "../components";
+import { startingFeatures } from "../constants";
+import styles from "../styles";
 
-export default GetStarted;
+import { staggerContainer , fadeIn, planetVariants } from "../utils/motion";
+
+
+const GetStarted = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants('left')}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
