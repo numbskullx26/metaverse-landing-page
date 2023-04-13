d95c6d34f03ce708fc4faadf0c4f5fab379973fc
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-13 12:30:06 +0530

    Update WhatsNew.jsx

--- starter_repo/sections/WhatsNew.jsx
+++ starter_repo/sections/WhatsNew.jsx
@@ -1,9 +1,21 @@
 'use client';
 
-const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
-);
+import { motion } from 'framer-motion';
 
-export default WhatsNew;
+import styles from '../styles';
+import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
+
+const WhatsNew = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+
+      
+    </motion.div>
