3e8ef018e1c5eb105d0587187790c5d31f09a5b0
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-14 12:04:41 +0530

    Update Footer.jsx

--- starter_repo/components/Footer.jsx
+++ starter_repo/components/Footer.jsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { socials } from '../constants';
+import { motion } from "framer-motion";
+import { socials } from "../constants";
 
-import styles from '../styles';
-import { footerVariants } from '../utils/motion';
-
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
 
 const Footer = () => (
   <motion.footer
@@ -20,7 +19,10 @@ const Footer = () => (
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
           Enter the Metaverse
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
           <img
             src="/headset.svg"
             alt="headset"
@@ -36,9 +38,24 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            METAVERUS
-          </h4>
+          <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
           </p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
+);
+
+export default Footer;
