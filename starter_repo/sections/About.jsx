c5bd446d99a4139c3f72e536dd0e190bc12fd00d
Indrajeet Sarkar <97308605+numbskullx26@users.noreply.github.com>
2023-04-14 14:55:40 +0530

    final commit

--- starter_repo/sections/About.jsx
+++ starter_repo/sections/About.jsx
@@ -1,50 +1,8 @@
-"use client";
-import { TypingText } from "../components";
-import styles from "../styles";
-
-import { fadeIn, staggerContainer } from "../utils/motion";
-
-import { motion } from "framer-motion";
+'use client';
 
 const About = () => (
-  <section className={`${styles.paddings} relative z-10`}>
-    <div className="gradient-02 z-0" />
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
-    >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
-      <motion.p
-        variants={fadeIn("up", "tween", 0.2, 1)}
-        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
-      >
-        <span className="font-extrabold text-white">The Metaverse,</span>
-        also stated as a virtual world is not just imagination but will be a
-        reality in the coming years.
-        <span className="font-extrabold text-white">The creation</span> of this
-        digital realm will be possible by collecting different technological
-        devices. Every task like shopping, playing, office work, meetings,
-        seminars, entertainment, etc could be easily done by us in this virtual
-        world. This will be possible by the efforts of companies like{" "}
-        <span className="font-extrabold text-white">Facebook, Microsoft</span>,
-        etc that are showing their interests in working on this concept to bring
-        the existence of the virtual world into reality. The movies like The
-        <span className="font-extrabold text-white">
-          {" "}
-          Matrix and Ready Player One
-        </span>
-        had given the representation of the virtual world in it.
-      </motion.p>
-      <motion.img
-        variants={fadeIn("up", "tween", 0.3, 1)}
-        src="/arrow-down.svg"
-        alt="arrow down"
-        className="w-[18px] h-[28px] object-contain mt-[28px]"
-      />
-    </motion.div>
+  <section>
+    About section
   </section>
 );
 
--- starter_repo/sections/Explore.jsx
+++ starter_repo/sections/Explore.jsx
@@ -1,49 +1,9 @@
-"use client";
+'use client';
 
-import { use, useState } from "react";
-import { TypingText, ExploreCard, TitleText } from "../components";
-import { exploreWorlds } from "../constants";
-import styles from "../styles";
-
-import { staggerContainer } from "../utils/motion";
-
-import { motion } from "framer-motion";
-
-const Explore = () => {
-  const [active, setActive] = useState("world-2");
-
-  return (
-    <section className={`${styles.paddings}`} id="explore">
-      <motion.div
-        variants={staggerContainer}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className={`${styles.innerWidth} mx-auto flex flex-col`}
-      >
-        <TypingText title="| The World" textStyles="text-center" />
-        <TitleText
-          title={
-            <>
-              Be careful <br className="md:block hidden" /> what you wish for!
-            </>
-          }
-          textStyles="text-center"
-        />
-        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
-          {exploreWorlds.map((world, index) => (
-            <ExploreCard
-              key={world.id}
-              {...world}
-              index={index}
-              active={active}
-              handleClick={setActive}
-            />
-          ))}
-        </div>
-      </motion.div>
-    </section>
-  );
-};
+const Explore = () => (
+  <section>
+    Explore section
+  </section>
+);
 
 export default Explore;
